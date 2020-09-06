d3060227eb70ea4f7d8605e28713632c535dcbbe
Sergei S <div@altertech.com>
2020-09-07 01:21:31 +0200

    removed debug stuff

--- .dev/release-upload.sh
+++ .dev/release-upload.sh
@@ -5,7 +5,7 @@ ID=$(curl -s https://api.github.com/repos/alttch/pulr/releases | jq -r .\[0\].id
 UPLOAD_ID=$(curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
     -H "Content-Type: application/gzip" \
     --data-binary @/tmp/pulr.x86_64.gz \
-    "https://uploads.github.com/repos/alttch/pulr/releases/${ID}/assets?name=pulr.x86_64.gza" | jq -r .id)
+    "https://uploads.github.com/repos/alttch/pulr/releases/${ID}/assets?name=pulr.x86_64.gz" | jq -r .id)
 [ -z "$UPLOAD_ID" ] && exit 1
 [ "$UPLOAD_ID" = "null" ] && exit 1
 echo "Uploaded"
