fc01516a8e52ba2e232ecd5b3206cbd9f1ecdb5f
Sergei S <div@altertech.com>
2020-10-05 03:00:28 +0200

    udp client, switched to rmodbus

--- src/proto/ppmodbus.rs
+++ src/proto/ppmodbus.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Deserializer};
 
-use modbus::{Client, Coil};
+use std::io::{Read, Write};
+use std::net::{SocketAddr, TcpStream, UdpSocket};
+
 use std::sync::mpsc;
 
 use ieee754::Ieee754;
@@ -13,6 +15,8 @@ use pl::IntervalLoop;
 
 use datatypes::{GenDataType, GenDataTypeParse, ParseData};
 use pl::datatypes;
+
+use rmodbus::{client::ModbusRequest, guess_response_frame_len, ModbusFrameBuf, ModbusProto};
 
 const DEFAULT_MODBUS_PORT: u16 = 502;
 
@@ -28,17 +32,22 @@ enum ModbusRegisterType {
 
 #[derive(Deserialize)]
 struct ModbusConfig {
-    proto: ModbusProto,
+    proto: ModbusProtocol,
     pull: Vec<ModbusPull>,
 }
 
 define_de_source!(DEFAULT_MODBUS_PORT);
 
+fn get_default_unit() -> u8 {
+    return 0;
+}
+
 #[derive(Deserialize)]
-struct ModbusProto {
+struct ModbusProtocol {
     name: String,
     #[serde(deserialize_with = "de_source")]
     source: HostPort,
+    #[serde(default = "get_default_unit")]
     unit: u8,
 }
 
@@ -47,6 +56,8 @@ struct ModbusPull {
     reg: String,
     count: u16,
     process: Vec<ModbusProcess>,
+    #[serde(default = "get_default_unit")]
+    unit: u8,
 }
 
 fn get_default_type() -> String {
@@ -64,58 +75,33 @@ struct ModbusProcess {
     transform: datatypes::EventTransformList,
 }
 
-// need coils as integers
-trait CoilToNumber {
-    fn to_number(&self) -> u16;
-}
-
-impl CoilToNumber for modbus::Coil {
-    fn to_number(&self) -> u16 {
-        return match self {
-            Coil::On => 1,
-            Coil::Off => 0,
-        };
-    }
-}
-
 // need to read coil data as Vec<u16>
 trait ModbusDataAsU16 {
-    fn read_coils_as_u16(&mut self, addr: u16, count: u16) -> Result<Vec<u16>, modbus::Error>;
-    fn read_discrete_inputs_as_u16(
+    fn parse_bool_as_u16(
         &mut self,
-        addr: u16,
-        count: u16,
-    ) -> Result<Vec<u16>, modbus::Error>;
+        response: &[u8],
+        result: &mut Vec<u16>,
+    ) -> Result<(), rmodbus::ErrorKind>;
 }
 
-impl ModbusDataAsU16 for modbus::tcp::Transport {
-    fn read_coils_as_u16(&mut self, addr: u16, count: u16) -> Result<Vec<u16>, modbus::Error> {
-        return match self.read_coils(addr, count) {
-            Ok(v) => {
-                let mut r = Vec::new();
-                for c in v {
-                    r.push(c.to_number());
-                }
-                Ok(r)
-            }
-            Err(e) => Err(e),
-        };
-    }
-    fn read_discrete_inputs_as_u16(
+impl ModbusDataAsU16 for ModbusRequest {
+    fn parse_bool_as_u16(
         &mut self,
-        addr: u16,
-        count: u16,
-    ) -> Result<Vec<u16>, modbus::Error> {
-        return match self.read_discrete_inputs(addr, count) {
-            Ok(v) => {
-                let mut r = Vec::new();
-                for c in v {
-                    r.push(c.to_number());
-                }
-                Ok(r)
-            }
-            Err(e) => Err(e),
-        };
+        response: &[u8],
+        result: &mut Vec<u16>,
+    ) -> Result<(), rmodbus::ErrorKind> {
+        let mut result_bool = Vec::new();
+        let r = self.parse_bool(response, &mut result_bool);
+        if r.is_err() {
+            return Err(r.err().unwrap());
+        }
+        for v in result_bool {
+            result.push(match v {
+                true => 1,
+                false => 0,
+            });
+        }
+        Ok(())
     }
 }
 
@@ -135,9 +121,273 @@ struct ModbusPullData {
     tp: ModbusRegisterType,
     addr: u16,
     count: u16,
+    unit: u8,
 }
 
 define_task_result!(Vec<u16>);
+
+#[derive(Debug, Eq, PartialEq)]
+enum ErrorKind {
+    ServerError,
+    UdpBindError,
+}
+
+struct TcpClient {
+    stream: std::net::TcpStream,
+    tr_id: u16,
+}
+
+struct UdpClient {
+    socket: std::net::UdpSocket,
+    tr_id: u16,
+    target: SocketAddr,
+}
+
+macro_rules! prepare_mreq {
+    ($unit_id:expr) => {
+        ModbusRequest::new($unit_id, ModbusProto::TcpUdp);
+    };
+}
+
+macro_rules! incr_tr_id {
+    ($self:expr) => {
+        $self.tr_id = match $self.tr_id {
+            u16::MAX => 0,
+            _ => $self.tr_id + 1,
+        }
+    };
+}
+
+impl TcpClient {
+    fn new(host: &str, port: u16, timeout: Duration) -> Result<Self, ErrorKind> {
+        let addr: SocketAddr = format!("{}:{}", host, port).parse().unwrap();
+        let stream = TcpStream::connect_timeout(&addr, timeout);
+        if stream.is_err() {
+            Err(ErrorKind::ServerError)
+        } else {
+            {
+                let s = stream.unwrap();
+                s.set_read_timeout(Some(timeout)).unwrap();
+                s.set_write_timeout(Some(timeout)).unwrap();
+                Ok(Self {
+                    stream: s,
+                    tr_id: 1,
+                })
+            }
+        }
+    }
+}
+
+impl UdpClient {
+    fn new(host: &str, port: u16, timeout: Duration) -> Result<Self, ErrorKind> {
+        let mut socket: Option<UdpSocket> = None;
+        let target: SocketAddr = format!("{}:{}", host, port).parse().unwrap();
+        for i in 20000..60000 {
+            let bind_addr: SocketAddr = format!("0.0.0.0:{}", i).parse().unwrap();
+            match UdpSocket::bind(bind_addr) {
+                Ok(v) => {
+                    socket = Some(v);
+                    break;
+                }
+                Err(_) => {}
+            }
+        }
+        if socket.is_none() {
+            Err(ErrorKind::UdpBindError)
+        } else {
+            {
+                let s = socket.unwrap();
+                s.set_read_timeout(Some(timeout)).unwrap();
+                s.set_write_timeout(Some(timeout)).unwrap();
+                Ok(Self {
+                    socket: s,
+                    tr_id: 1,
+                    target: target,
+                })
+            }
+        }
+    }
+}
+
+trait ModbusNetworkClient {
+    fn process_request(&mut self, request: &[u8]) -> Result<Vec<u8>, rmodbus::ErrorKind>;
+    fn prepare(&mut self);
+}
+
+impl ModbusNetworkClient for TcpClient {
+    fn process_request(&mut self, request: &[u8]) -> Result<Vec<u8>, rmodbus::ErrorKind> {
+        if self.stream.write(&request).is_err() {
+            return Err(rmodbus::ErrorKind::CommunicationError);
+        }
+        let mut buf = [0u8; 6];
+        if self.stream.read_exact(&mut buf).is_err() {
+            return Err(rmodbus::ErrorKind::CommunicationError);
+        }
+        let mut response = Vec::new();
+        response.extend_from_slice(&buf);
+        let len = guess_response_frame_len(&buf, ModbusProto::TcpUdp).unwrap();
+        if len > 6 {
+            let mut rest = vec![0u8; (len - 6) as usize];
+            if self.stream.read_exact(&mut rest).is_err() {
+                return Err(rmodbus::ErrorKind::CommunicationError);
+            }
+            response.extend(rest);
+        }
+        Ok(response)
+    }
+    fn prepare(&mut self) {
+        incr_tr_id!(self);
+    }
+}
+
+impl ModbusNetworkClient for UdpClient {
+    fn process_request(&mut self, request: &[u8]) -> Result<Vec<u8>, rmodbus::ErrorKind> {
+        let len = match self.socket.send_to(request, self.target) {
+            Ok(v) => v,
+            Err(_) => return Err(rmodbus::ErrorKind::CommunicationError),
+        };
+        if len != request.len() {
+            return Err(rmodbus::ErrorKind::CommunicationError);
+        }
+        let mut buf: ModbusFrameBuf = [0; 256];
+        let (n, _addr) = match self.socket.recv_from(&mut buf) {
+            Ok(v) => v,
+            Err(_) => {
+                return Err(rmodbus::ErrorKind::CommunicationError);
+            }
+        };
+        let mut response = Vec::new();
+        response.extend_from_slice(&buf[..n]);
+        Ok(response)
+    }
+    fn prepare(&mut self) {
+        incr_tr_id!(self);
+    }
+}
+
+macro_rules! prepare_client {
+    ($client:expr) => {
+        match $client {
+            ModbusClient::Tcp(c) => c.prepare(),
+            ModbusClient::Udp(c) => c.prepare(),
+        };
+    };
+}
+
+macro_rules! process_request {
+    ($client:expr, $request:expr) => {
+        match $client {
+            ModbusClient::Tcp(c) => c.process_request($request),
+            ModbusClient::Udp(c) => c.process_request($request),
+        }
+    };
+}
+
+fn read_coils(
+    client: &mut ModbusClient,
+    unit_id: u8,
+    reg: u16,
+    count: u16,
+) -> Result<Vec<u16>, rmodbus::ErrorKind> {
+    prepare_client!(client);
+    let mut mreq = prepare_mreq!(unit_id);
+    let mut request = Vec::new();
+    match mreq.generate_get_coils(reg, count, &mut request) {
+        Ok(_) => {}
+        Err(e) => return Err(e),
+    }
+    let response = match process_request!(client, &request) {
+        Ok(v) => v,
+        Err(e) => {
+            return Err(e);
+        }
+    };
+    let mut result = Vec::new();
+    match mreq.parse_bool_as_u16(&response, &mut result) {
+        Ok(_) => Ok(result),
+        Err(e) => Err(e),
+    }
+}
+fn read_discretes(
+    client: &mut ModbusClient,
+    unit_id: u8,
+    reg: u16,
+    count: u16,
+) -> Result<Vec<u16>, rmodbus::ErrorKind> {
+    prepare_client!(client);
+    let mut mreq = prepare_mreq!(unit_id);
+    let mut request = Vec::new();
+    match mreq.generate_get_discretes(reg, count, &mut request) {
+        Ok(_) => {}
+        Err(e) => return Err(e),
+    }
+    let response = match process_request!(client, &request) {
+        Ok(v) => v,
+        Err(e) => {
+            return Err(e);
+        }
+    };
+    let mut result = Vec::new();
+    match mreq.parse_bool_as_u16(&response, &mut result) {
+        Ok(_) => Ok(result),
+        Err(e) => Err(e),
+    }
+}
+fn read_inputs(
+    client: &mut ModbusClient,
+    unit_id: u8,
+    reg: u16,
+    count: u16,
+) -> Result<Vec<u16>, rmodbus::ErrorKind> {
+    prepare_client!(client);
+    let mut mreq = prepare_mreq!(unit_id);
+    let mut request = Vec::new();
+    match mreq.generate_get_inputs(reg, count, &mut request) {
+        Ok(_) => {}
+        Err(e) => return Err(e),
+    }
+    let response = match process_request!(client, &request) {
+        Ok(v) => v,
+        Err(e) => {
+            return Err(e);
+        }
+    };
+    let mut result = Vec::new();
+    match mreq.parse_u16(&response, &mut result) {
+        Ok(_) => Ok(result),
+        Err(e) => Err(e),
+    }
+}
+fn read_holdings(
+    client: &mut ModbusClient,
+    unit_id: u8,
+    reg: u16,
+    count: u16,
+) -> Result<Vec<u16>, rmodbus::ErrorKind> {
+    prepare_client!(client);
+    let mut mreq = prepare_mreq!(unit_id);
+    let mut request = Vec::new();
+    match mreq.generate_get_holdings(reg, count, &mut request) {
+        Ok(_) => {}
+        Err(e) => return Err(e),
+    }
+    let response = match process_request!(client, &request) {
+        Ok(v) => v,
+        Err(e) => {
+            return Err(e);
+        }
+    };
+    let mut result = Vec::new();
+    match mreq.parse_u16(&response, &mut result) {
+        Ok(_) => Ok(result),
+        Err(e) => Err(e),
+    }
+}
+
+enum ModbusClient {
+    Tcp(TcpClient),
+    Udp(UdpClient),
+}
 
 pub fn run(
     inloop: bool,
@@ -150,20 +400,19 @@ pub fn run(
 ) {
     // process config
     let config: ModbusConfig = serde_yaml::from_str(&cfg).unwrap();
-    // TODO: move to de_
-    if config.proto.name != "modbus/tcp" {
-        unimplemented!("protocol {}", config.proto.name);
-    }
     // connect Modbus client
-    let cfg = modbus::tcp::Config {
-        modbus_uid: config.proto.unit,
-        tcp_port: config.proto.source.port,
-        tcp_connect_timeout: Some(timeout),
-        tcp_read_timeout: Some(timeout),
-        tcp_write_timeout: Some(timeout),
+    let mut client: ModbusClient = match config.proto.name.as_str() {
+        "modbus/tcp" => ModbusClient::Tcp(
+            TcpClient::new(&config.proto.source.host, config.proto.source.port, timeout)
+                .expect("unable to connect to server"),
+        ),
+        "modbus/udp" => ModbusClient::Udp(
+            UdpClient::new(&config.proto.source.host, config.proto.source.port, timeout).unwrap(),
+        ),
+        _ => {
+            unimplemented!("protocol {}", config.proto.name);
+        }
     };
-    let mut client = modbus::tcp::Transport::new_with_cfg(&config.proto.source.host, cfg)
-        .expect("unable to connect to source");
     // create pulls and dp list
     let mut pulls: Vec<ModbusPullData> = Vec::new();
     let mut dp_list: Vec<Vec<ModbusDataProcessInfo>> = Vec::new();
@@ -193,10 +442,18 @@ pub fn run(
                 transform: prc.transform,
             });
         }
+        let mut unit = p.unit;
+        if unit == 0 {
+            unit = config.proto.unit;
+        }
+        if unit == 0 {
+            panic!("Modbus unit not specified, neither in pull config, nor default");
+        }
         pulls.push(ModbusPullData {
             tp: register_type,
             addr: addr as u16,
             count: p.count,
+            unit: unit,
         });
         dp_list.push(process_data_vec);
     }
@@ -333,10 +590,12 @@ pub fn run(
                 pl::print_debug(&format!("reading registers {:?}", p));
             }
             let data = match p.tp {
-                ModbusRegisterType::Holding => client.read_holding_registers(p.addr, p.count),
-                ModbusRegisterType::Input => client.read_input_registers(p.addr, p.count),
-                ModbusRegisterType::Coil => client.read_coils_as_u16(p.addr, p.count),
-                ModbusRegisterType::Discrete => client.read_discrete_inputs_as_u16(p.addr, p.count),
+                ModbusRegisterType::Holding => read_holdings(&mut client, p.unit, p.addr, p.count),
+                ModbusRegisterType::Input => read_inputs(&mut client, p.unit, p.addr, p.count),
+                ModbusRegisterType::Coil => read_coils(&mut client, p.unit, p.addr, p.count),
+                ModbusRegisterType::Discrete => {
+                    read_discretes(&mut client, p.unit, p.addr, p.count)
+                }
             };
             tx.send(match data {
                 Ok(v) => {
@@ -349,7 +608,7 @@ pub fn run(
                         t: call_time,
                     }
                 }
-                Err(err) => panic!("{}", err),
+                Err(err) => panic!("{:?}", err),
             })
             .unwrap();
         }
@@ -360,5 +619,4 @@ pub fn run(
         pull_loop.sleep()
     }
     terminate_processor!(processor, tx);
-    client.close().expect("client disconnected with an error");
 }
