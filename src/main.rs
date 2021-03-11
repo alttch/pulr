4b40ac77460ea68a595ffacb6be567e659c11455
Sergei S <div@altertech.com>
2021-03-11 19:35:41 +0200

    1.0.8

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ mod ppenip;
 mod ppsnmp;
 
 const HOMEPAGE: &str = "https://github.com/alttch/pulr";
-const VERSION: &str = "1.0.7";
+const VERSION: &str = "1.0.8";
 
 fn get_default_timeout() -> f32 {
     return 5.0;
